77bde5b4c89a54db3de7188f8b63a0e036339341
vanshika8484 <vanshikaagarwal8484@gmail.com>
2025-06-20 10:10:21 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [todolist, setTodolist] = useState([]);
+  let saveToDoList = (event) => {
+    let toname = event.target.toname.value;
+    if (!todolist.includes(toname)) {
+      let finalToDoList = [...todolist, toname];
+      setTodolist(finalToDoList);
+    } else {
+      alert("to do name already exist...");
+    }
+    event.preventDefault();
+  };
+  let list = todolist.map((value, index) => {
+    return (
+      <ToDoListItems
+        key={index}
+        value={value}
+        indexNumber={index}
+        todolist={todolist}
+        setTodolist={setTodolist}
+      />
+    );
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="Heading">To Do List</h1>
+      <form onSubmit={saveToDoList}>
+        <input type="text" name="toname"></input>
+        <button>Save</button>
+      </form>
+      <div className="outerDiv">
+        <ul>{list}</ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItems({ value, indexNumber, todolist, setTodolist }) {
+  let [status, setStatus] = useState(false);
+  let deleteRow = () => {
+    let finalData = todolist.filter((v, i) => i != indexNumber);
+    setTodolist(finalData);
+  };
+  let checkStatus = () => {
+    setStatus(!status);
+  };
+  return (
+    <li className={status ? "completetodo" : ""} onClick={checkStatus}>
+      {indexNumber + 1}
+      {". " + value}
+      <span onClick={deleteRow}>&times;</span>
+    </li>
+  );
+}
